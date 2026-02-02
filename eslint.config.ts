07c041c6231c06754f90fbebf275c5339186ccae
Jiahao Yuan <kahojyun@icloud.com>
2026-02-02 17:53:34 +0800

    Add ESLint caching and React linting plugins

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  js.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+  reactX.configs["recommended-typescript"],
+  reactDom.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+]);
