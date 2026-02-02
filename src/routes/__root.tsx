97bfb8bc53ae5e7e60ad2bdd59ba97bdb2988f37
Jiahao Yuan <kahojyun@icloud.com>
2026-02-02 19:58:08 +0800

    Update dependencies and fix TanStack Router devtools package name

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const queryClient = new QueryClient();
 
